b8a96dd350055b11232f71bd29906357125916ed
The-Sal <70804343+The-Sal@users.noreply.github.com>
2021-12-11 09:16:12 +0400

    ...

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 reqs = ['utils-s', 'pydub']
-version = '2.9'
+version = '3.0'
 
 setuptools.setup(
     name='playerFramework',
     version=version,
-    description="A Bridge between compiled players and Python",
+    description="A simple way to play audio in Python.",
     packages=setuptools.find_packages(),
     install_requires=reqs
 )
